dbb9749dca04c985513273e6200772f0dee85b07
Jimmy Lai <noahsark@yahoo-inc.com>
2014-11-04 10:48:15 +0800

    add ALICESIE

---
+++ classical/ALICESIE.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf8 -*-
+'''Module docstring
+'''
+
+__author__ = 'noahsark'
+
+import sys
+
+num = int(sys.stdin.readline())
+for i in range(num):
+    N = int(sys.stdin.readline())
+    print (N + 1) / 2
